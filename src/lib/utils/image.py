d434d6f8383df3a2ba44fd23716ea7340b634507
Ping Chao <pingchao@umich.edu>
2020-08-18 23:38:09 +0800

    speedup

--- src/lib/utils/image.py
+++ src/lib/utils/image.py
@@ -17,10 +17,8 @@ def flip(img):
   return img[:, :, ::-1].copy()  
 
 def transform_preds(coords, center, scale, output_size):
-    target_coords = np.zeros(coords.shape)
     trans = get_affine_transform(center, scale, 0, output_size, inv=1)
-    for p in range(coords.shape[0]):
-        target_coords[p, 0:2] = affine_transform(coords[p, 0:2], trans)
+    target_coords = affine_transform(coords.T, trans).T
     return target_coords
 
 
